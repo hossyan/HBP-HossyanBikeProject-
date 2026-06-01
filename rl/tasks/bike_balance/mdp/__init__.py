adeecf8085136245cb5da8917f2a0b4dc7c99f63
hossyan <kohossyan@gmail.com>
2026-06-01 10:43:46 +0900

    速度型PIの追加

--- rl/tasks/bike_balance/mdp/__init__.py
+++ rl/tasks/bike_balance/mdp/__init__.py
@@ -7,3 +7,4 @@ from .observations import ComplementaryRollFilter, RollRateObservation
 from .rewards import body_roll_reward, body_roll_vel_penalty, back_tire_vel_penalty
 from .terminations import roll_exceeded
 from .events import set_joint_position_target
+from .actions import BldcVelocityPiActionTermCfg, BldcVelocityPiActionTerm
